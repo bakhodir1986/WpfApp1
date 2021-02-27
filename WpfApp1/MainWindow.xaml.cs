da4ac61f9773adf7ad87b96cc45750981ea41468
Bakhodir Radjabov <bakhodir.radjabov@gmail.com>
2021-02-28 00:28:51 +0500

    DataGridView

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Data;
 using System.Windows.Media;
 using System.Windows.Shapes;
 using WpfApp1.Model;
@@ -78,6 +81,8 @@ namespace WpfApp1
                 CreateHeaderOfPivodTable(pivodModelList);
 
                 FillDataToPivodTable(pivodModelList);
+
+                FillDataToDataGridPivodTable(pivodModelList);
             }
         }
 
@@ -108,6 +113,38 @@ namespace WpfApp1
 
                 row++;
                 col = 3;
+            }
+        }
+
+        private void FillDataToDataGridPivodTable(List<PivodTableModel> pivodModelList)
+        {
+            int col = 3;
+            int row = 3;
+
+            foreach (var pivod in pivodModelList)
+            {
+                dynamic rowToAdd = new ExpandoObject();
+
+                var colValue = string.Format("Column{0}", 0);
+                ((IDictionary<String, Object>)rowToAdd)[colValue] = pivod.TourId.ToString();
+                colValue = string.Format("Column{0}", 1);
+                ((IDictionary<String, Object>)rowToAdd)[colValue] = pivod.JointGroups;
+                colValue = string.Format("Column{0}", 2);
+                ((IDictionary<String, Object>)rowToAdd)[colValue] = pivod.FromStartTillEndDateWithDash;
+
+                foreach (var dictCell in pivod.TourPlacesModels)
+                {
+                    colValue = string.Format("Column{0}", col);
+                    ((IDictionary<String, Object>)rowToAdd)[colValue] = dictCell.Value;
+
+                    col++;
+                }
+
+                PivodDataGrid.Items.Add(rowToAdd);
+
+                row++;
+                col = 3;
+
             }
         }
 
@@ -144,6 +181,22 @@ namespace WpfApp1
             PivodGrid.Children.Add(txtBlockCellN);
         }
 
+        private void PrintTextToCell(Grid grid, string printValue, int row, int col)
+        {
+            TextBlock txtBlockCellN = new TextBlock();
+            txtBlockCellN.Text = printValue;
+            txtBlockCellN.FontSize = 14;
+            txtBlockCellN.FontWeight = FontWeights.Bold;
+            txtBlockCellN.Foreground = new SolidColorBrush(DefaultTextColor);
+            txtBlockCellN.VerticalAlignment = VerticalAlignment.Center;
+            txtBlockCellN.HorizontalAlignment = HorizontalAlignment.Center;
+
+            Grid.SetRow(txtBlockCellN, row);
+            Grid.SetColumn(txtBlockCellN, col);
+
+            grid.Children.Add(txtBlockCellN);
+        }
+
         private void BrushCellOfPivod(SolidColorBrush color, int row, int col)
         {
             Rectangle rectN = new Rectangle();
@@ -177,6 +230,11 @@ namespace WpfApp1
         {
             PivodGrid.ShowGridLines = ShowGridLines;
             PivodGrid.Background = new SolidColorBrush(TableBackgroundColor);
+
+            PivodDataGrid.FrozenColumnCount = 3;
+            PivodDataGrid.HorizontalScrollBarVisibility = ScrollBarVisibility.Visible;
+            PivodDataGrid.VerticalScrollBarVisibility = ScrollBarVisibility.Visible;
+            PivodDataGrid.IsReadOnly = true;
         }
 
         private void CreateHeaderOfPivodTable(List<PivodTableModel> pivodModelList)
@@ -192,12 +250,15 @@ namespace WpfApp1
         {
             // Add first column header    
             PrintTextToCell("Номер", 2, 0);
+            CreateDataGridColumn(FirstColumnWidthPixels, string.Empty, string.Empty, "Номер", "Column0");
 
             // Add second column header    
             PrintTextToCell("Сборные группы", 2, 1);
+            CreateDataGridColumn(SecondColumnWidthPixels, string.Empty, string.Empty, "Сборные группы", "Column1");
 
             // Add third column header    
             PrintTextToCell("Даты проведения", 2, 2);
+            CreateDataGridColumn(ThirdColumnWidthPixels, string.Empty, string.Empty, "Даты проведения", "Column2");
 
             var columnDic = pivodModelList.First().TourPlacesModels;
 
@@ -210,6 +271,11 @@ namespace WpfApp1
                 PrintTextToCell(dictValue.Key.Day.ToString(), 1, col);
 
                 PrintTextToCell(((WeekDays) dictValue.Key.DayOfWeek).ToString(), 2, col);
+
+                CreateDataGridColumn(PivodColumnsWidthPixes, ((YearMonthes)dictValue.Key.Month).ToString()
+                    , dictValue.Key.Day.ToString()
+                    , ((WeekDays)dictValue.Key.DayOfWeek).ToString()
+                    , string.Format("Column{0}", col));
 
                 col++;
             }
@@ -240,9 +306,51 @@ namespace WpfApp1
             PivodGrid.RowDefinitions.Add(gridRow1);
         }
 
+        private void CreateRowDefinision(Grid grid, int height)
+        {
+            RowDefinition gridRow1 = new RowDefinition();
+            gridRow1.Height = new GridLength(height);
+            grid.RowDefinitions.Add(gridRow1);
+        }
+
         private void CreateColumnDefinision(int width)
         {
             PivodGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(width) });
+        }
+
+        private void CreateDataGridColumn(int width, string text1, string text2, string text3, string colBindingText)
+        {
+            DataTemplate dataTemplate = new DataTemplate();
+
+            FrameworkElementFactory gridFactory = new FrameworkElementFactory(typeof(Grid));
+            FrameworkElementFactory row1 = new FrameworkElementFactory(typeof(RowDefinition));
+            FrameworkElementFactory row2 = new FrameworkElementFactory(typeof(RowDefinition));
+            FrameworkElementFactory row3 = new FrameworkElementFactory(typeof(RowDefinition));
+
+            row1.SetValue(RowDefinition.HeightProperty, new GridLength(1, GridUnitType.Auto));
+            row2.SetValue(RowDefinition.HeightProperty, new GridLength(1, GridUnitType.Auto));
+            row3.SetValue(RowDefinition.HeightProperty, new GridLength(1, GridUnitType.Auto));
+            gridFactory.AppendChild(row1);
+            gridFactory.AppendChild(row2);
+            gridFactory.AppendChild(row3);
+
+            FrameworkElementFactory tBlockFactory1 = new FrameworkElementFactory(typeof(TextBlock));
+            tBlockFactory1.SetValue(Grid.ColumnProperty, 2);
+            tBlockFactory1.SetValue(FrameworkElement.VerticalAlignmentProperty, VerticalAlignment.Center);
+            tBlockFactory1.SetValue(TextBlock.TextProperty
+                , string.Format("{0}                                   {1}                           {2}", text1, text2, text3));
+            tBlockFactory1.SetValue(TextBlock.TextWrappingProperty, TextWrapping.Wrap);
+            tBlockFactory1.SetValue(TextBlock.FontWeightProperty, FontWeights.Bold);
+
+            gridFactory.AppendChild(tBlockFactory1);
+
+            dataTemplate.VisualTree = gridFactory;
+
+            DataGridTextColumn c1 = new DataGridTextColumn();
+            c1.Width = width;
+            c1.HeaderTemplate = dataTemplate;
+            c1.Binding = new Binding(colBindingText);
+            PivodDataGrid.Columns.Add(c1);
         }
     }
 }
